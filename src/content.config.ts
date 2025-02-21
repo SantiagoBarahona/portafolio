fe8dd6db3fcc50bc4b1c7cb9f1df69727acd1d9e
santiago <llsanthiago@gmail.com>
2025-02-21 14:32:26 -0500

    Prettier setted

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,19 @@
-import { defineCollection, z } from 'astro:content';
-import { file } from 'astro/loaders'
+import { defineCollection, z } from "astro:content";
+import { file } from "astro/loaders";
 
 const projectSchema = z.object({
-    title: z.string(),
-    description: z.string(),
-    stack: z.array(z.string()),
-    href: z.string(),
-    imgSrc: z.string(),
-    github: z.string()
-})
+  title: z.string(),
+  description: z.string(),
+  stack: z.array(z.string()),
+  href: z.string(),
+  imgSrc: z.string(),
+  github: z.string(),
+});
 
 const projects = defineCollection({
-    loader: file('src/data/projects.json'),
-    schema: projectSchema
-})
+  loader: file("src/data/projects.json"),
+  schema: projectSchema,
+});
 
 export type Project = z.infer<typeof projectSchema>;
-export const collections = { projects }
+export const collections = { projects };
--- src/data/Links.js
+++ src/data/Links.js
@@ -1,3 +1,4 @@
-export const githubRoute = 'https://github.com/SantiagoBarahona'
-export const linkedInRoute = 'https://www.linkedin.com/in/santiago-zapata-barahona-4b489320b/'
-export const xRoute = 'https://x.com/UribeEsUnHP' 
+export const githubRoute = "https://github.com/SantiagoBarahona";
+export const linkedInRoute =
+  "https://www.linkedin.com/in/santiago-zapata-barahona-4b489320b/";
+export const xRoute = "https://x.com/UribeEsUnHP";
