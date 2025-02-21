c70a31cb23cc874d80ffe3c0de51ade9fd502176
santiago <llsanthiago@gmail.com>
2025-02-21 13:39:16 -0500

    Borrador de la página

---
+++ src/data/Links.js
@@ -0,0 +1,3 @@
+export const githubRoute = 'https://github.com/SantiagoBarahona'
+export const linkedInRoute = 'https://www.linkedin.com/in/santiago-zapata-barahona-4b489320b/'
+export const xRoute = 'https://x.com/UribeEsUnHP' 
