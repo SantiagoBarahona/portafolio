b95b985f82596c7cc2cc6ec1938e6ed10a690574
Santiago Zapata Barahona <llsanthiago@gmail.com>
2025-02-23 22:41:49 -0500

    refactor: replace direct links with SocialChip component and remove unused Links.js

--- src/data/Links.js
+++
@@ -1,4 +0,0 @@
-export const githubRoute = "https://github.com/SantiagoBarahona";
-export const linkedInRoute =
-  "https://www.linkedin.com/in/santiago-zapata-barahona-4b489320b/";
-export const xRoute = "https://x.com/UribeEsUnHP";
